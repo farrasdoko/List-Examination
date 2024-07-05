892f89b4dfbc8990f98505389eb01f7cc814657d
farrasdoko <farras.abiyyu.doko@gmail.com>
2024-07-06 00:27:45 +0700

    Initial Commit

---
+++ List Examination/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  List Examination
+//
+//  Created by Farras on 06/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
