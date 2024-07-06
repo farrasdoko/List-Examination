86af3dafeaf2efa100406d11fc82d8db947e6de5
farrasdoko <farras.abiyyu.doko@gmail.com>
2024-07-06 12:56:48 +0700

    Pull To Refresh

--- List Examination/Screens/Main/MainVC.swift
+++ List Examination/Screens/Main/MainVC.swift
@@ -9,16 +9,23 @@ import UIKit
 
 class MainVC: UIViewController {
     
+    // MARK: - Data variables
     let allData = [
-        "Data"
+        "Data", "Diti", "Dutu", "Dete", "Doto"
     ]
+    var displayedData: [String] = []
     
+    // MARK: - Views
     let tableView = UITableView()
+    let refreshControl = UIRefreshControl()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupTableView()
+        setupRefreshControl()
+        
+        loadData()
     }
     
     private func setupTableView() {
@@ -27,23 +34,46 @@ class MainVC: UIViewController {
         tableView.delegate = self
         view.addSubview(tableView)
     }
+    
+    func loadData() {
+        DispatchQueue.global().async {
+            self.displayedData = self.allData
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    // MARK: - Pull to Refresh
+    private func setupRefreshControl() {
+        refreshControl.addTarget(self, action: #selector(refreshData), for: .valueChanged)
+        tableView.refreshControl = refreshControl
+    }
+    @objc func refreshData() {
+        displayedData.removeAll()
+        
+        loadData()
+        
+        refreshControl.endRefreshing()
+    }
+    
 }
 
 extension MainVC: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return allData.count
+        return displayedData.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .default, reuseIdentifier: "Cell")
         
-        cell.textLabel?.text = allData[indexPath.row]
+        cell.textLabel?.text = displayedData[indexPath.row]
         
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        print("You tapped on \(allData[indexPath.row])")
+        print("You tapped on \(displayedData[indexPath.row])")
     }
 }
