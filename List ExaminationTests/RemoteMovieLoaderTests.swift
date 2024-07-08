7fca787b822405b26848f53e801aef3a58094bfa
farrasdoko <farras.abiyyu.doko@gmail.com>
2024-07-08 10:04:28 +0700

    Implement Abstract Test on Networking

--- List ExaminationTests/RemoteMovieLoaderTests.swift
+++ List ExaminationTests/RemoteMovieLoaderTests.swift
@@ -8,11 +8,39 @@
 import XCTest
 
 class RemoteMovieLoader {
+    let client: HTTPClient
     
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
+    func load() {
+        client.get(from: URL(string: "https://api.themoviedb.org/")!)
+    }
+}
+
+class HTTPClient {
+    var url: URL?
+    
+    func get(from url: URL) {
+        self.url = url
+    }
 }
 
 class RemoteMovieLoaderTests: XCTestCase {
-    func test_init() {
-        let sut = RemoteMovieLoader()
+    func test_init_doesNotRequestDataFromClient() {
+        let client = HTTPClient()
+        _ = RemoteMovieLoader(client: client)
+        
+        XCTAssertNil(client.url)
+    }
+    
+    func test_load_requestDataFromUrl() {
+        let client = HTTPClient()
+        let sut = RemoteMovieLoader(client: client)
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.url)
     }
 }
